d26f8d56ef42b8df7a3730d140ff07c29d2206ff
Isabela Gomes <belaisag17@gmail.com>
2026-05-07 19:13:46 -0300

    Add files via upload

---
+++ VariaveisPontoflutuante.java
@@ -0,0 +1,21 @@
+package com.loiane.cursojava.aula11;
+
+public class VariaveisPontoflutuante {
+
+	public static void main(String[] args) {
+		
+		double valorPassagem = 5.90; 
+		
+		float valorTomate = 3.95F; 
+		
+		System.out.println("Valor da passagem = " + valorPassagem);
+		System.out.println("Valor do Tomate = " + valorTomate);
+		
+		
+			
+	
+		
+
+	}
+
+}
